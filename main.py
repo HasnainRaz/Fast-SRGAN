aaed0375f32678467f6e17810f5ef1a274bcc6ec
Hasnain <hasnain3257@gmail.com>
2020-10-11 21:25:19 +0200

    use val dir

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 
 import hydra
+import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
-import torch
+
 from dataloader import SuperResolutionDataset
 from pl_module import FastSRGAN
 
 
 @hydra.main(config_path='configs', config_name='super_resolution')
 def main(cfg):
-    image_paths = sorted([os.path.join(cfg.DATA.IMAGE_DIR, x) for x in os.listdir(cfg.DATA.IMAGE_DIR)])
-    num_train = int(len(image_paths) * 0.95)
-    train_image_paths = image_paths[:num_train]
-    val_image_paths = image_paths[num_train:]
+    train_image_paths = sorted(
+        [os.path.join(cfg.DATA.TRAIN_IMAGE_DIR, x) for x in os.listdir(cfg.DATA.TRAIN_IMAGE_DIR)])
+    val_image_paths = sorted([os.path.join(cfg.DATA.VAL_IMAGE_DIR, x) for x in os.listdir(cfg.DATA.VAL_IMAGE_DIR)])
     train_loader = DataLoader(SuperResolutionDataset(train_image_paths, cfg.DATA.HIGH_RES_IMAGE_SIZE),
                               batch_size=cfg.TRAINING.BATCH_SIZE,
                               shuffle=True,
