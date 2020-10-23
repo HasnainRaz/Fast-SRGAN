824ec99aab8ae67f496ad46254755639192d17de
HasnainRaz <hasnain3257@gmail.com>
2020-10-23 21:20:01 +0200

    Commit and backend changes

--- main.py
+++ main.py
@@ -24,14 +24,15 @@ def main(cfg):
                             shuffle=False,
                             num_workers=cfg.DATA.NUM_WORKERS)
     checkpoint_callback = ModelCheckpoint(monitor='SSIM/Generated', mode='max')
-    early_stop_callback = EarlyStopping(monitor='SSIM/Generated', mode='max', patience=20)
+    early_stop_callback = EarlyStopping(monitor='SSIM/Generated', mode='max', patience=cfg.TRAINING.PATIENCE)
     module = FastSRGAN(cfg.MODEL)
     module.example_input_array = torch.rand((1, 3, 256, 256))
     trainer = Trainer(max_epochs=cfg.TRAINING.EPOCHS,
                       gpus=cfg.TRAINING.GPUS,
                       callbacks=[early_stop_callback],
                       checkpoint_callback=checkpoint_callback,
-                      precision=cfg.TRAINING.PRECISION)
+                      precision=cfg.TRAINING.PRECISION,
+                      accelerator='ddp')
     trainer.fit(module, train_loader, val_loader)
 
 
