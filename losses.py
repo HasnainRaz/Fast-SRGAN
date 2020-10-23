6ec8517dacffdf4ecfa8a2d91c073611744a3fc8
hasnain <hasnain3257@gmail.com>
2020-10-24 00:07:42 +0200

    fix loss

--- losses.py
+++ losses.py
@@ -9,10 +9,10 @@ class RALoss(nn.Module):
         self.loss_fn = nn.BCEWithLogitsLoss()
 
     def forward(self, real_out, fake_out):
-        ones = torch.ones(real_out.shape).type_as(real_out)
         zeros = torch.zeros(real_out.shape).type_as(real_out)
+        ones = torch.ones(real_out.shape).type_as(real_out)
 
         a = real_out - torch.mean(fake_out, dim=0)
         b = fake_out - torch.mean(real_out, dim=0)
-        loss = (self.loss_fn(a, zeros) + self.loss_fn(b, ones)) * 0.5
+        loss = (self.loss_fn(a, ones) + self.loss_fn(b, zeros)) * 0.5
         return loss
