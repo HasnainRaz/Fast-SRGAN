7c2e7c65d1c53c9f163efcf2a207e7b94a9f7445
David Lee <davlee1972@yahoo.com>
2022-12-14 15:44:24 -0800

    fixed infer.py

--- infer.py
+++ infer.py
@@ -7,7 +7,23 @@ import os
 parser = ArgumentParser()
 parser.add_argument('--image_dir', type=str, help='Directory where images are kept.')
 parser.add_argument('--output_dir', type=str, help='Directory where to output high res images.')
+  
+def change_model(model, new_input_shape, custom_objects=None):
+    # replace input shape of first layer    
+    model.layers[0]._batch_input_shape = new_input_shape
 
+    # rebuild model architecture by exporting and importing via json
+    new_model = keras.models.model_from_json(model.to_json(),custom_objects=custom_objects)
+
+    # copy weights from old model to new one
+    for layer in new_model.layers:
+        try:
+            layer.set_weights(model.get_layer(name=layer.name).get_weights())
+            print("Loaded layer {}".format(layer.name))
+        except:
+            print("Could not transfer weights for layer {}".format(layer.name))
+
+    return new_model
 
 def main():
     args = parser.parse_args()
@@ -17,9 +33,7 @@ def main():
 
     # Change model input shape to accept all size inputs
     model = keras.models.load_model('models/generator.h5')
-    inputs = keras.Input((None, None, 3))
-    output = model(inputs)
-    model = keras.models.Model(inputs, output)
+    model = change_model(model, new_input_shape=[None, None, None, 3])
 
     # Loop over all images
     for image_path in image_paths:
